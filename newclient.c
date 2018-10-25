7ee003013bfc0ef2a4a7ed1e7c9869cb0cfc6bdd
Komalpreet Maan <komalmann899@gmail.com>
2018-10-25 20:26:32 +1000

    Client works too!

--- newclient.c
+++ newclient.c
@@ -3,14 +3,14 @@
 
 //Include Header files
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
+#include<string.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<signal.h>
 
-#define MAX_MESSAGE_SIZE 2000
+#define MAXIMUM_MES_SIZE 2000
 
 //Define program variables
 int socket_desc; //Socket descriptor
@@ -62,42 +62,43 @@ void leader_board(){
 
 }
 
-int main(int argc, char * argv[]){
-  signal(SIGINT, handle_signal);
-  int read_size;
-  char * ip = argv[1];
-  //Check ip
-  if(ip = NULL){
-  printf("IP not provided\n");
-  exit(EXIT_FAILURE);
-  }
-  //Check IP format
-  if((inet_pton(AF_INET, ip, &(server.sin_addr.s_addr))) == 0){
-    printf("IP format invalid\n");
-    exit(EXIT_FAILURE);
-  }
-  int port = port_number(argv[2]);
-  //set up socket
-  socket_desc = socket(AF_INET, SOCK_STREAM,0);
-  if (socket_desc < 0) {
+int main(int argc, char *argv[]){
+	char * ip = argv[1];
+	int port = port_number(argv[2]);
+	int read_size;
+if(ip == NULL) {
+        printf("IP has not been provided\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((inet_pton(AF_INET, ip, &(server.sin_addr.s_addr))) == 0) {
+        printf("IP is not a valid format\n");
+        exit(EXIT_FAILURE);
+    }
+    // set up socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    // create socket
+
+    if (socket_desc < 0) {
         printf("Could not create socket\n");
         exit(EXIT_FAILURE);
     } else {
         printf("Socket created\n");
     }
     // Prepare the sockaddr_in structure
-    server.sin_family = AF_INET; // set IP address
+    server.sin_family = AF_INET; // set up IPV4 address
+    // broadcast on any available IP that belongs to the host machine
     server.sin_addr.s_addr = inet_addr(ip);
     server.sin_port = htons(port);// set port address to program argument
 
-    if (connect(socket_desc, (struct sockaddr *)&server , sizeof(server)) < 0) {
+   if (connect(socket_desc, (struct sockaddr *)&server , sizeof(server)) < 0) {
         perror("connect failed. Error");
         exit(EXIT_FAILURE);
     } else {
         // Check if the connection to the server is open once connecting
         // if it isn't the server has disconnected from the client
-        char * reply = (char *)malloc(MAX_MESSAGE_SIZE * sizeof(char));
-        read_size = recv(socket_desc , reply , MAX_MESSAGE_SIZE , 0);
+        char * reply = (char *)malloc(MAXIMUM_MES_SIZE * sizeof(char));
+        read_size = recv(socket_desc , reply , MAXIMUM_MES_SIZE , 0);
         if (read_size <= 0) {
             puts("Server full, closing application");
             shutdown(socket_desc, 2);
