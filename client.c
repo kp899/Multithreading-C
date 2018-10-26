04ee89f39bcdd3452634cd11ee05c07d7852d0b7
Komalpreet Maan <komalmann899@gmail.com>
2018-10-26 22:09:17 +1000

    Revert "need to sync between two  os's"
    This reverts commit 4b2e2d6b33fe7f8218d26886b5ef9934f92915ac.

--- client.c
+++ client.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 #include<string.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
@@ -8,23 +7,6 @@
 #include<signal.h>
 
 #define MAX_MESSAGE_SIZE 2000
-#define NUM_MINES 10
-#define NUM_TILES_X 9
-#define NUM_TILES_Y 9
-#define BORDER_LENGTH 9
-#define DEBUG 0
-#define TILES_PER_ROW 9
-
-typedef struct {
-  int adjacent_mines;
-  bool revealed;
-  bool is_flagged;
-} Tile;
-
-typedef struct {
-  Tile tiles[NUM_TILES_X][NUM_TILES_Y];
-} GameView;
-char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 int game_state = -1;
 int socket_desc;
@@ -35,30 +17,6 @@ char * trim_new_line(char * input_str) {
     input_str[len - 1] = '\0';
     return input_str;
 }
-int *recv_int_array(int connection, int size) {
-    int number_of_bytes, i=0;
-    uint32_t value;
-
-	int *results = malloc(sizeof(int)*size);
-	for (i=0; i < size; i++) {
-		if ((number_of_bytes=recv(connection, &value, sizeof(uint32_t), 0))
-		         == -1) {
-			perror("recv");
-			exit(1);
-
-		}
-		results[i] = ntohl(value);
-	}
-	return results;
-}
-void send_int_array(int connection, int *array, int size) {
-	uint32_t value;
-	for (int i = 0; i < size; i++) {
-		value = htonl(array[i]);
-		send(connection, &value, sizeof(uint32_t), 0);
-	}
-}
-
 void login_screen() {
     username = (char *)malloc(20 * sizeof(char));
     char * password = (char *)malloc(6 * sizeof(char));
@@ -96,171 +54,6 @@ void login_screen() {
         printf("\n===============================================\n\n\n");
         game_state = 0;
     }
-}
-void draw_border() {
-	for(int i=0; i < BORDER_LENGTH; i++) printf("========");
-	printf("\n");
-}
-void clear_buffer() {
-	while ((getchar()) != '\n');
-}
-//Converts an integer or
-int char_to_int(char ch, char type, int limit) {
-  	if (type == 'c') {
-  		for(int i=0; alphabet[i]; i++) {
-  			if (alphabet[i] == ch) {
-  				if ((i % 26) < limit) return (i % 26);
-  				return -1;
-  			}
-  		}
-  	} else {
-  		return atoi(&ch)-1;
-  	}
-  	return -1;
-}
-//Return the numerical value of the alphabet letter
-int get_input_letter(int limit) {
-	char val = '\0';
-	scanf("%c", &val);
-	clear_buffer();
-	for(int i=0; alphabet[i]; i++) {
-			if (alphabet[i] == val) {
-				if ((i % 26) < limit) return (i % 26);
-				return -1;
-			}
-	}
-	return -1;
-}
-//takes a number char and return it's integer value-1
-int get_input_number() {
-	char val = '\0';
-	scanf("%c", &val);
-	clear_buffer();
-	return atoi(&val)-1;
-}
-//Send a 3 int array to the server
-void send_instruction(int connection, int a, int x, int y) {
-	int array[3] = {a , x, y};
-	send_int_array(connection, array, 3);
-}
-//Draws the game tiles in the mine field
-int draw_gameview(int connection) {
-	draw_border();
-
-	//Receive game status from the server
-	int *status = recv_int_array(connection, 3);
-	if (DEBUG) printf("Recvd Status\n");
-	//Get tile values
-	int *field = recv_int_array(connection, TILES_PER_ROW*TILES_PER_ROW);
-	if (DEBUG) printf("Recvd Tiles\n");
-
-	//Displaying mines left and current game time
-	printf("\nRemaining Mines: %d\n", status[1]);
-
-	//Draw mine field
-	printf("\n     "); //Print numbers at top
-	for (int i=1; i<= TILES_PER_ROW; i++) {
-		printf("%d ", i);
-	}
-	printf("\n "); //Printing the top margin
-	for (int i=0; i< 2*TILES_PER_ROW+4; i++) {
-		printf("-");
-	}
-	int val;
-	for (int i=0; i< TILES_PER_ROW; i++) {
-		printf("\n %c | ", alphabet[i]);
-		for (int x=0; x < TILES_PER_ROW; x++) {
-			val = field[x+(i*TILES_PER_ROW)];
-			if (val == -1) {
-				printf("  ");
-			} else if (val == -2) {
-				printf("+ ");
-			} else if (val == -3) {
-				printf("* ");
-			} else {
-				printf("%d ", val);
-			}
-		}
-	}
-	printf("\n");
-	fflush(stdout);
-
-	//If the game is over, check to see if player won
-	if (status[0] == 0) {
-		if (status[1] == 0) {
-			printf("\nCongradulations! You found all the mines!\n");
-			printf("You won in %d seconds!\n", status[2]);
-		} else {
-			printf("\nGame Over! You hit a mine.\n");
-			printf("The game lasted %d seconds.\n", status[2]);
-		}
-		return 1; //Returns to the main menu
-	}
-
-	printf("\nChoose an Option:\n");
-	printf("<R> Reveal a Tile\n");
-	printf("<P> Place a Flag\n");
-	printf("<Q> Quit Game\n");
-
-	free(status);
-
-	//Get input and action game menu
-	int input;
-		//Get the option selected
-		do {
-			printf("\nOption (R, P, Q): ");
-			input = get_input_letter(26);
-			if (DEBUG) printf("Imput value %d\n",input);
-
-			if ((input < 15) || (input > 17)) {//Input is not Q R or P
-				printf("Please enter one of the listed options.\n");
-				input = -1;
-			}
-		} while (input == -1);
-
-		//If P was selected - Place Flag
-		if (input == 15) {
-			int x,y;
-			printf("\nPlacing a Flag!");
-			do {
-				printf("\nEnter the tile coordinates: ");
-				char val[2];
-				scanf("%2s", val);
-				clear_buffer();
-				y = char_to_int(val[0],'c',9);
-				x = char_to_int(val[1],'n',9);
-				if ((x == -1)||(y == -1)) printf("\nPlease enter the coordinates (e.g. A2)");
-			} while ((x == -1)||(y == -1));
-
-			//Place flag at x,y.
-			send_instruction(connection, 2, x, y);
-		}
-
-		//If Q was selected - Quit Game
-		if (input == 16) {
-			printf("\nReturning to Main Menu...\n");
-			return 1;
-		}
-
-		//If R was selected - Reveal Tile
-		if (input == 17) {
-			int x,y;
-			printf("\nRevealing a Tile!");
-			do {
-				printf("\nEnter the tile coordinates: ");
-				char val[2];
-				scanf("%2s", val);
-				clear_buffer();
-				y = char_to_int(val[0],'c',9);
-				x = char_to_int(val[1],'n',9);
-				if ((x == -1)||(y == -1)) printf("\nPlease enter the coordinates (e.g. A2)");
-			} while ((x == -1)||(y == -1));
-
-			//Reveal tile at x,y.
-			send_instruction(connection, 3, x, y);
-		}
-
-	return 2;
 }
 
 void menu() {
@@ -309,12 +102,12 @@ void menu() {
     }
 }
 
-void handle_signit(int sig) {
+void handle_signit(int sig) { 
     printf("SIGINT detected %d\n", sig);
     printf("Exiting has begun.");
     close(socket_desc);
     exit(EXIT_SUCCESS);
-}
+} 
 
 int get_port_no(char* input) {
     int num = 0;
@@ -394,9 +187,6 @@ int main(int argc, char * argv[]) {
 
         if (game_state == 0) {
             menu();
-        }
-        if(game_state == 1){
-          draw_gameview();
         }
     }
     close(socket_desc);
